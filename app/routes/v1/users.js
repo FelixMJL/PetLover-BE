ce2c5c672592796c9b6efdcf6c8e15f2a39a51d1
Teng Liu <ericliu940430@gmail.com>
2023-02-16 17:12:07 +1100

    eric 2/16/2023 user

--- app/routes/v1/users.js
+++ app/routes/v1/users.js
@@ -1,11 +1,11 @@
-const express = require('express');
 const users = require("../../controllers/users");
 const {Router} = require("express");
-// const router = new express.Router();
 
 const userRouter = Router();
 
 userRouter.get('', users.getAllUsers);
-
+userRouter.get('/:id', users.getUserById);
+userRouter.post('', users.UserRegistration);
+userRouter.put('', users.UserProfileEdit);
 
 module.exports = userRouter;
