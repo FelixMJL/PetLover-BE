ce6807b48501b91bbfba9b01fb579a976b6e4656
minshen <kuangminshen@gmail.com>
2023-02-16 16:05:36 +1100

    conflict handle

--- app/controllers/comments.js
+++ app/controllers/comments.js
@@ -15,6 +15,17 @@ exports.addAComment = async (req, res) => {
   }
 };
 
+// Find a comment with user details
+exports.getAComment = async (req, res) => {
+  const { id } = req.params;
+  const comment = await CommentModel.findById(id).populate('comment_by').exec();
+  if (!comment) {
+    res.status(404).json({ error: 'comment not found' });
+    return;
+  }
+  res.json(comment);
+};
+
 exports.deleteAComment = async (req, res) => {
   try {
     const commentId = req.params.id;
