1cafc81e7a3519c86dc3914d0c7e598b897d0c62
WeidongFang1985 <fangweidong1985@gmail.com>
2023-02-16 15:49:53 +1100

    conflict handle

--- app/routes/v1/comments.js
+++ app/routes/v1/comments.js
@@ -3,10 +3,7 @@ const { Router } = require('express');
 
 const commentRouter = Router();
 
-<<<<<<< HEAD
 commentRouter.delete('/:id', comments.deleteAComment);
-=======
 commentRouter.post('/', comments.addAComment);
->>>>>>> 869bad86aa71d671a90b99b1b9889c4911499c80
 
 module.exports = commentRouter;
