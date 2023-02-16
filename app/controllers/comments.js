62a7c0e48eb1e07a8d52fb7b0df3662911d31333
minshen <kuangminshen@gmail.com>
2023-02-16 12:23:14 +1100

    delete console log

--- app/controllers/comments.js
+++ app/controllers/comments.js
@@ -4,14 +4,10 @@ exports.addAComment = async (req, res) => {
   try {
     const newComment = new CommentModel(req.body);
     const currentPost = await PostModel.findById(req.body.comment_to);
-    // console.log(newComment);
-    // console.log(currentPost);
     const savedComment = await newComment.save();
-
     await currentPost.updateOne({
       $push: { comments: newComment },
     });
-    // console.log(currentPost);
     res.status(200).json(savedComment);
   } catch (err) {
     res.status(404).json('comment can not be added');
