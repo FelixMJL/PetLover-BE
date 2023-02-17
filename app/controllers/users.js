b6bc273a8f45d658317c90cf8d7365d05feec50b
Teng Liu <ericliu940430@gmail.com>
2023-02-17 16:38:04 +1100

    new fix by Eric 2/17/2023

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -2,80 +2,68 @@ const UserModel = require('../models/user')
 
 // Find all users with posts spread  PL-44
 exports.getAllUsers = async (req, res) => {
-    const users =await UserModel.find().populate('posts').exec()
+    const users = await UserModel.find().populate('posts').exec()
     res.json(users)
 }
 
 // Find a user by ID with his all posts spread PL-43
 exports.getUserById = async (req, res) => {
-    let id = req.params.id
-    user = await UserModel.findById(id).populate('posts following followers').exec()
-    res.json(user)
+    try{
+        const { id } = req.params
+        const user = await UserModel.findById(id).populate('posts').exec()
+        if (!user) {
+            res.status(404).json({ error: "user not exist" })
+        }
+        res.json(user)
+    }catch(e){
+        res.status(404).json({error:"id must be a string of 12 bytes or a string of 24 hex characters or an integer"})
+    }
 }
 
 //users registration by username password nickname email PL-41
 exports.UserRegistration = async (req, res) => {
-    const {
-        username,
-        password,
-        nickname,
-        email,
-    } = req.body
-    //get all users username and E-mail first and export as object 
-    const allUsers = await UserModel.find({}, "username email -_id").exec()
-    //avoid  duplicate username and E-mail
-    const user = allUsers.find(user => user.username === username || user.email === email)
-    if (user && user.username === username) {
-        res.send(`User name --${username}-- has been used please change another one`)
-    } else if (user && user.email === email) {
-        res.send(`E-mail --${email}-- has been used please change another one`)
-    } else {
-        UserModel.create([
-            {
-                username: `${username}`,
-                password: `${password}`,
-                nickname: `${nickname}`,
-                email: `${email}`,
-            }
-        ], (err, doc) => {
-            if (!err) {
-                res.status(201).json(doc)
-            }
-        })
+    try{
+        const {
+            username,
+            password,
+            nickname,
+            email,
+        } = req.body
+        //get all users username and E-mail first and export as object 
+        const allUsers = await UserModel.find({}, "username email -_id").exec()
+        //avoid  duplicate username and E-mail
+        const user = allUsers.find(user => user.username === username || user.email === email)
+        if (user && user.username === username) {
+            res.send(`User name --${username}-- has been used please change another one`)
+        } else if (user && user.email === email) {
+            res.send(`E-mail --${email}-- has been used please change another one`)
+        } else {
+            const newUser = new UserModel({username, nickname, email, password});
+            console.log(newUser);
+            await newUser.hashPassword();
+            await newUser.save();
+            res.status(201).json(newUser)
+        }
+    }catch (e) {
+        res.status(500).json(e)
     }
 }
 
 
 //Edit user profile PL-42
 exports.UserProfileEdit = async (req, res) => {
-    const { id } = req.params
-    const {
-        username,
-        password,
-        nickname,
-        email,
-        location,
-        avatar,
-        introduction,
-        website_url,
-    } = req.body
-
-
-    const allUsers = await UserModel.find({}, "username email -_id").exec()
-    //avoid  duplicate username and E-mail
-    const user = allUsers.find(user => user.username === username || user.email === email)
-    if (user && user.username === username) {
-        res.send(`User name --${username}-- has been used please change another one`)
-    } else if (user && user.email === email) {
-        res.send(`E-mail --${email}-- has been used please change another one`)
-    } else {
+    try{
+        const { id } = req.params
+        const {
+            location,
+            avatar,
+            introduction,
+            website_url,
+        } = req.body
+    
         const userUpdated = await UserModel.findByIdAndUpdate(
             id,
             {
-                username,
-                password,
-                nickname,
-                email,
                 location,
                 avatar,
                 introduction,
@@ -90,5 +78,7 @@ exports.UserProfileEdit = async (req, res) => {
             return
         }
         res.json(userUpdated)
+    }catch (e) {
+        res.status(404).json({error:"id must be a string of 12 bytes or a string of 24 hex characters or an integer"})
     }
 }
