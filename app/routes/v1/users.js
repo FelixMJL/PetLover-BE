65cd530d8c57577b2621d636f61df0dabb933ea6
jiaweiaaa <50084033+jiaweiaaa@users.noreply.github.com>
2023-02-21 22:37:05 +1100

    change params name

--- app/routes/v1/users.js
+++ app/routes/v1/users.js
@@ -9,8 +9,8 @@ userRouter.use(authGuard)
 userRouter.get('', users.getAllUsers);
 userRouter.get('/:id', users.getUserById);
 userRouter.put('/:id', users.UserProfileEdit);
-userRouter.post('/:followedId/following/:followerId', users.followAUser);
-userRouter.delete('/:followedId/following/:followerId', users.unfollowAUser);
+userRouter.post('/:followerId/following/:followedId', users.followAUser);
+userRouter.delete('/:followerId/following/:followedId', users.unfollowAUser);
 userRouter.get('/:id/following', users.getAllFollowingsOfAUser)
 userRouter.get('/:id/follower', users.getAllFollowersOfAUser)
 
