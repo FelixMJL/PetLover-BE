32305cfdc56abd0d537db257ce9d85e8a7214197
WeidongFang1985 <fangweidong1985@gmail.com>
2023-02-23 02:25:02 +1100

    add a comment bug fixed

--- app/middleware/tokenErrorHandler.js
+++
@@ -1,5 +0,0 @@
-module.exports = (err, req, res, next) => {
-    if(err.message === "jwt malformed")
-    res.status(400).json({error: "no token, please login in first"});
-    next(err)
-}
--- app/routes/v1/comments.js
+++ app/routes/v1/comments.js
@@ -6,9 +6,7 @@ const commentValidation = require("../../validators/commentValidation")
 const commentRouter = Router();
 
 commentRouter.post('/comments', authGuard, commentValidation.store, comments.store);
-commentRouter.post('/replies', authGuard, replyValidation.store, comments.store);
 commentRouter.get('/comments/:id', authGuard, comments.show);
-commentRouter.get('/replies/:id', authGuard, comments.showReply);
 commentRouter.delete('/comments/:id', authGuard, comments.delete);
 
 module.exports = commentRouter;
