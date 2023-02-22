3e43491aa6e3ee9c43a71d243bed2fa7ea86d1e8
WeidongFang1985 <fangweidong1985@gmail.com>
2023-02-22 11:57:17 +1100

    Refactor routes

--- app/routes/v1/comments.js
+++ app/routes/v1/comments.js
@@ -1,17 +1,13 @@
 const comments = require('../../controllers/comments');
-const { Router } = require('express');
+const {Router} = require('express');
 const authGuard = require("../../middleware/authGuards");
 const replyValidation = require("../../validators/replyValidation")
 const commentValidation = require("../../validators/commentValidation")
-
 const commentRouter = Router();
 
-commentRouter.use(authGuard)
-commentRouter.post('', commentValidation.store, comments.store);
-commentRouter.post('/replies', replyValidation.store, comments.store);
-commentRouter.get('/:id', comments.show);
-commentRouter.delete('/:id', comments.delete);
-
-
+commentRouter.post('/comments', authGuard, commentValidation.store, comments.store);
+commentRouter.post('/comments/replies', authGuard, replyValidation.store, comments.store);
+commentRouter.get('/comments/:id', authGuard, comments.show);
+commentRouter.delete('/comments/:id', authGuard, comments.delete);
 
 module.exports = commentRouter;
--- app/routes/v1/users.js
+++ app/routes/v1/users.js
@@ -1,17 +1,16 @@
 const users = require("../../controllers/users");
 const {Router} = require("express");
-const userRouter = Router();
 const authGuard = require("../../middleware/authGuards");
+const userRouter = Router();
 
-userRouter.post('', users.UserRegistration);
-userRouter.post('/login', users.userLogin);
-userRouter.use(authGuard)
-userRouter.get('', users.getAllUsers);
-userRouter.get('/:id', users.getUserById);
-userRouter.put('/:id', users.UserProfileEdit);
-userRouter.post('/:currentUserId/following/:targetUserId', users.followAUser);
-userRouter.delete('/:currentUserId/unfollowing/:targetUserId', users.unfollowAUser);
-userRouter.get('/:id/following', users.getAllFollowingsOfAUser)
-userRouter.get('/:id/follower', users.getAllFollowersOfAUser)
+userRouter.post('/users', users.registration);
+userRouter.post('/users/login', users.login);
+userRouter.get('/users', authGuard,users.index);
+userRouter.get('/users/:id', authGuard,users.show);
+userRouter.put('/users/:id', authGuard, users.update);
+userRouter.post('/users/:currentUserId/following/:targetUserId', authGuard, users.followAUser);
+userRouter.delete('/users/:currentUserId/unfollowing/:targetUserId', authGuard, users.unfollowAUser);
+userRouter.get('/users/:id/following', authGuard, users.getAllFollowingsOfAUser)
+userRouter.get('/users/:id/follower', authGuard, users.getAllFollowersOfAUser)
 
 module.exports = userRouter;
