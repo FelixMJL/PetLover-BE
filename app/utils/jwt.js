fcb720906cacf1fd764d2f97a89faa32d6d7589b
Teng Liu <ericliu940430@gmail.com>
2023-02-23 21:43:34 +1100

    PL-49 PL-50 fix by Eric 2/23/2023

--- app/utils/jwt.js
+++ app/utils/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const JWT_KEY = "petLover";
+const JWT_KEY = process.env.JWT_KEY;
 
 const generateToken = (payload) => {
     return jwt.sign(payload, JWT_KEY, {expiresIn: '24h'});
