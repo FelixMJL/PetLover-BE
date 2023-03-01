c3806255eee50b33e48224db08f8337ddda51154
WeidongFang1985 <fangweidong1985@gmail.com>
2023-03-01 20:34:39 +1100

    fix login change payload from email to id

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -5,50 +5,51 @@ const {validationResult} = require("express-validator");
 //users registration by username password nickname email PL-41 PL-49
 //POST localhost:3000/api/v1/users
 exports.register = async (req, res) => {
-        const {username, password, nickname, email} = req.body;
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            res.status(422).json({errors: errors})
-            return
-        }
-        if (await UserModel.findOne({username}).exec()) {
-            res.send(`User name --${username}-- has been used please change another one`)
-            return;
-        }
+    const {username, password, nickname, email} = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(422).json({errors: errors})
+        return
+    }
+    if (await UserModel.findOne({username}).exec()) {
+        res.send(`User name --${username}-- has been used please change another one`)
+        return;
+    }
 
-        if (await UserModel.findOne({email}).exec()) {
-            res.send(`E-mail --${email}-- has been used please change another one`)
-            return;
-        }
+    if (await UserModel.findOne({email}).exec()) {
+        res.send(`E-mail --${email}-- has been used please change another one`)
+        return;
+    }
 
-        const user = new UserModel({username, nickname, email, password});
-        await user.hashPassword();
-        await user.save();
-        const token = generateToken({id: user.id, email}); // roles: []
-        res.status(201).json({email, token});
+    const user = new UserModel({username, nickname, email, password});
+    await user.hashPassword();
+    await user.save();
+    const token = generateToken({id: user.id, email}); // roles: []
+    res.status(201).json({email, token});
 }
 
 // user login PL-50
 // POST localhost:3000/api/v1/users/login
 exports.login = async (req, res) => {
-        const {email, password} = req.body
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            res.status(422).json({errors: errors})
-            return
-        }
-        const user = await UserModel.findOne({email}).exec()
-        if (!user) {
-            res.status(401).json({error: 'Invalid email or password'});
-            return;
-        }
+    const {email, password} = req.body
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(422).json({errors: errors})
+        return
+    }
+    const user = await UserModel.findOne({email}).exec()
+    if (!user) {
+        res.status(401).json({error: 'Invalid email or password'});
+        return;
+    }
 
-        if (!await user.validatePassword(password)) {
-            res.status(401).json({error: 'Invalid email or password'});
-            return;
-        }
-        const token = generateToken({id: user.id, email}); // roles: []
-        res.status(201).json({email, token});
+    if (!await user.validatePassword(password)) {
+        res.status(401).json({error: 'Invalid email or password'});
+        return;
+    }
+    const token = generateToken({id: user.id, email});
+    const id = user.id;
+    res.status(201).json({id, token});
 }
 
 
@@ -62,13 +63,13 @@ exports.index = async (req, res) => {
 // Find a user by ID with his all posts spread PL-43
 // GET localhost:3000/api/v1/users/:id
 exports.show = async (req, res) => {
-        const {id} = req.params
-        const user = await UserModel.findById(id).populate('posts').exec()
-        if (!user) {
-            res.status(404).json({error: "User not exist"})
-            return
-        }
-        res.json(user)
+    const {id} = req.params
+    const user = await UserModel.findById(id).populate('posts').exec()
+    if (!user) {
+        res.status(404).json({error: "User not exist"})
+        return
+    }
+    res.json(user)
 }
 
 //Edit user profile PL-42
@@ -108,69 +109,69 @@ exports.update = async (req, res) => {
 }
 //Follow a user PL-52
 exports.followAUser = async (req, res) => {
-        const {currentUserId, targetUserId} = req.params
-        const currentUser = await UserModel.findById(currentUserId).exec()
-        const targetUser = await UserModel.findById(targetUserId).exec()
-        if (!currentUser || !targetUser) {
-            res.status(404).json({error: 'User not found'});
-            return;
-        }
-        targetUser.followers.addToSet(currentUserId);
-        await targetUser.save();
-        currentUser.following.addToSet(targetUserId);
-        await currentUser.save();
-        res.status(201).json(currentUser)
+    const {currentUserId, targetUserId} = req.params
+    const currentUser = await UserModel.findById(currentUserId).exec()
+    const targetUser = await UserModel.findById(targetUserId).exec()
+    if (!currentUser || !targetUser) {
+        res.status(404).json({error: 'User not found'});
+        return;
+    }
+    targetUser.followers.addToSet(currentUserId);
+    await targetUser.save();
+    currentUser.following.addToSet(targetUserId);
+    await currentUser.save();
+    res.status(201).json(currentUser)
 }
 
 //unfollow a user PL-53
 exports.unfollowAUser = async (req, res) => {
-        const {currentUserId, targetUserId} = req.params
-        let currentUser = await UserModel.findById(currentUserId).exec()
-        const targetUser = await UserModel.findById(targetUserId).exec()
-        if (!currentUser || !targetUser) {
-            res.status(404).json({error: 'User not found'});
-            return;
+    const {currentUserId, targetUserId} = req.params
+    let currentUser = await UserModel.findById(currentUserId).exec()
+    const targetUser = await UserModel.findById(targetUserId).exec()
+    if (!currentUser || !targetUser) {
+        res.status(404).json({error: 'User not found'});
+        return;
+    }
+    currentUser = await UserModel.findByIdAndUpdate(currentUserId, {
+        $pull: {following: targetUserId},
+    }, {new: true}).exec();
+    await UserModel.findByIdAndUpdate(
+        targetUserId,
+        {
+            $pull: {
+                followers: currentUserId,
+            },
         }
-        currentUser = await UserModel.findByIdAndUpdate(currentUserId, {
-            $pull: {following: targetUserId},
-        }, {new: true}).exec();
-        await UserModel.findByIdAndUpdate(
-            targetUserId,
-            {
-                $pull: {
-                    followers: currentUserId,
-                },
-            }
-        ).exec();
-        res.status(201).json(currentUser)
+    ).exec();
+    res.status(201).json(currentUser)
 }
 
 // show one user's followings
 exports.getAllFollowingsOfAUser = async (req, res) => {
-        const {id} = req.params
-        const user = await UserModel.findById(id)
-        const followings = await UserModel.findById(id).populate(
-            'following',
-            {avatar: 1, username: 1, nickname: 1, introduction: 1}
-        ).exec()
-        if (!user) {
-            res.status(404).json({error: "User not exist"})
-            return
-        }
-        res.json(followings)
+    const {id} = req.params
+    const user = await UserModel.findById(id)
+    const followings = await UserModel.findById(id).populate(
+        'following',
+        {avatar: 1, username: 1, nickname: 1, introduction: 1}
+    ).exec()
+    if (!user) {
+        res.status(404).json({error: "User not exist"})
+        return
+    }
+    res.json(followings)
 }
 
 // show one user's followers
 exports.getAllFollowersOfAUser = async (req, res) => {
-        const {id} = req.params
-        const user = await UserModel.findById(id)
-        const followers = await UserModel.findById(id).populate(
-            'followers',
-            {avatar: 1, username: 1, nickname: 1, introduction: 1}
-        ).exec()
-        if (!user) {
-            res.status(404).json({error: "User not exist"})
-            return
-        }
-        res.json(followers)
+    const {id} = req.params
+    const user = await UserModel.findById(id)
+    const followers = await UserModel.findById(id).populate(
+        'followers',
+        {avatar: 1, username: 1, nickname: 1, introduction: 1}
+    ).exec()
+    if (!user) {
+        res.status(404).json({error: "User not exist"})
+        return
+    }
+    res.json(followers)
 }
